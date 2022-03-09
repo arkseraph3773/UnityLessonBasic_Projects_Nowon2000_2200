68e11fea73561c379f8733e0f663ebf2519a1cc4
arkseraph3773 <arkseraph3773@gmail.com>
2022-03-09 21:58:41 +0900

    0309
    0309
    gamePlay
    noteManager
    noteSpawner

--- RhythmGame/Assets/02.Scripts/SongSelector.cs
+++ RhythmGame/Assets/02.Scripts/SongSelector.cs
@@ -5,20 +5,31 @@ using UnityEngine.Video;
 
 public class SongSelector : MonoBehaviour
 {
-    public VideoPlayer vp;
-    public SongData songData;
+    public static SongSelector Instance;
+    private void Awake()
+    {
+        if(Instance != null)
+        {
+            Destroy(Instance);
+        }
+        Instance = this;
+        DontDestroyOnLoad(Instance);
+    }
+    public bool isPlayable
+    {
+        get 
+        { 
+            return songData != null && clip != null; 
+        }
+    }
+    //public VideoPlayer vp; //곡선택시 화면 나오게 하려면 써라
+    [HideInInspector] public VideoClip clip;
+    [HideInInspector] public SongData songData;
     public void LoadSong(string videoName)
     {
-        VideoClip vc = Resources.Load<VideoClip>(videoName);
-        Debug.Log(vc);
-        vp.clip = vc;
-        TextAsset songDataText = Resources.Load<TextAsset>(videoName);
-        Debug.Log(songDataText);
+        clip = Resources.Load<VideoClip>($"VideoClips/{videoName}");
+        TextAsset songDataText = Resources.Load<TextAsset>($"SongDatas/{videoName}");
         songData = JsonUtility.FromJson<SongData>(songDataText.ToString());
-        Debug.Log(songData);
     }
-    public void TestVideoPlay()
-    {
-        vp.Play();
-    }
+    
 }
