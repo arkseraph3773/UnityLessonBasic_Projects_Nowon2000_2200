43df4db3d12e5b232e0caa49aac34bb1f89d1f99
arkseraph3773 <arkseraph3773@gmail.com>
2022-04-01 21:52:41 +0900

    0401
    0401
    PlayerStateMachine_Idle
    PlayerStateMachine_Run
    PlayerStateMachine_Jump
    PlayerStateMachineManager

--- Runner/Assets/02.Scripts/Player/PlayerStateMachine.cs
+++ Runner/Assets/02.Scripts/Player/PlayerStateMachine.cs
@@ -1,19 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class PlayerStateMachine : MonoBehaviour
 {
     public State state;
+    public PlayerState playerState;
     public KeyCode keyCode;
-    public Animator animator;
+    [HideInInspector] public Animator animator;
+    [HideInInspector] public PlayerStateMachineManager manager;
 
-    private void Awake()
+
+    public virtual void Awake()
     {
         animator = GetComponent<Animator>();
+        manager = GetComponent<PlayerStateMachineManager>();
     }
+
     // 머신 동작 끝났는지 체크
-    public bool isFinished
+    public bool isFinihsed
     {
         get
         {
@@ -22,9 +26,8 @@ public class PlayerStateMachine : MonoBehaviour
     }
 
     /// <summary>
-    /// 머신동작 가능여부 체크
+    /// 머신 동작 가능 여부 체크
     /// </summary>
-    
     public virtual bool IsExecuteOK()
     {
         return true;
@@ -37,17 +40,29 @@ public class PlayerStateMachine : MonoBehaviour
     {
         state = State.Prepare;
     }
-    
+
     /// <summary>
     /// 머신 동작 업데이트
     /// </summary>
-    public virtual void UpdateState()
+    public virtual PlayerState UpdateState()
     {
-        switch(state)
+        PlayerState nextPlayerState = playerState;
+        switch (state)
         {
             case State.Idle:
                 break;
+            case State.Prepare:
+                break;
+            case State.Casting:
+                break;
+            case State.OnAction:
+                break;
+            case State.Finish:
+                break;
+            default:
+                break;
         }
+        return nextPlayerState;
     }
 
     /// <summary>
@@ -58,6 +73,7 @@ public class PlayerStateMachine : MonoBehaviour
         state = State.Idle;
     }
 
+
     public enum State
     {
         Idle,
@@ -67,4 +83,3 @@ public class PlayerStateMachine : MonoBehaviour
         Finish
     }
 }
-
