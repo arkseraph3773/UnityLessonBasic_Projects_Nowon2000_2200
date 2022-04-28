2d3d405c7b3852bcc09f99449a174829822f0a3a
arkseraph3773 <arkseraph3773@gmail.com>
2022-04-28 21:57:13 +0900

    0428
    0428
    Item Rooting

--- RPG/Assets/02.Scripts/Item/ItemController.cs
+++ RPG/Assets/02.Scripts/Item/ItemController.cs
@@ -16,8 +16,6 @@ public class ItemController : MonoBehaviour
     public float popForce = 1f;
     public float rotateSpeed = 1f;
 
-    
-
     [Header("Kinematics")]
     private Rigidbody rb;
     private BoxCollider col;
@@ -27,10 +25,10 @@ public class ItemController : MonoBehaviour
     private Vector3 rendererOffset;
     private float elapsedFixedTime;
     private Vector3 eulerAngleOrigin;
-    private Coroutine coroutine = null; 
-    //=========================================================================
-    //***************************Public Methods *******************************
-    //=========================================================================
+    private Coroutine coroutine = null;
+    //====================================================================
+    //************************** Public Methods **************************
+    //====================================================================
 
     public void PickUp(Player player)
     {
@@ -41,9 +39,9 @@ public class ItemController : MonoBehaviour
         }
     }
 
-    //=========================================================================
-    //***************************Private Methods ******************************
-    //=========================================================================
+    //====================================================================
+    //************************** Private Methods *************************
+    //====================================================================
 
     private void Awake()
     {
@@ -59,18 +57,15 @@ public class ItemController : MonoBehaviour
         elapsedFixedTime = 0;
         StartCoroutine(E_ShowEffect());
     }
-
     private void FixedUpdate()
     {
         if (doFloatingEffect)
-        {
             Floating();
-        }
     }
 
     private void Floating()
     {
-        rendererTransform.localPosition = rendererOffset + 
+        rendererTransform.localPosition = rendererOffset +
                                           new Vector3(0f,
                                                       floatingHeight * Mathf.Sin(floatingSpeed * elapsedFixedTime),
                                                       0f);
@@ -82,16 +77,13 @@ public class ItemController : MonoBehaviour
         rb.velocity = Vector3.zero;
         rb.AddForce(Vector3.up * popForce, ForceMode.Impulse);
 
+        Debug.Log("Imma rotating");
         while (doFloatingEffect == false)
         {
-            Collider[] grounds = Physics.OverlapSphere(rb.position - new Vector3(0f, col.size.y / 2, 0f), 0.3f, groundLayer);
+            Collider[] grounds = Physics.OverlapSphere(rb.position - new Vector3(0f, col.size.y / 2, 0f), 0.1f, groundLayer);
 
-            if(grounds.Length > 0)
-            {
+            if (grounds.Length > 0)
                 doFloatingEffect = true;
-                rb.velocity = Vector3.zero;
-                break;
-            }
 
             rendererTransform.Rotate(new Vector3(rotateSpeed * Time.deltaTime, 0f, rotateSpeed * Time.deltaTime));
             yield return null;
@@ -103,43 +95,54 @@ public class ItemController : MonoBehaviour
     {
         doFloatingEffect = false;
         rb.velocity = Vector3.zero;
-        rb.AddForce(Vector3.up * popForce, ForceMode.Impulse);
-        yield return new WaitForEndOfFrame();
+        //rb.AddForce(Vector3.up * popForce, ForceMode.VelocityChange);
 
         rb.useGravity = false;
         bool isReachedToPlayer = false;
+        CharacterController playerCol = player.GetComponent<CharacterController>();
+        Vector3 playerOffset = new Vector3(0,
+                                            playerCol.height / 2 + playerCol.radius,
+                                            0);
 
         MeshRenderer meshRenderer = rendererTransform.GetComponent<MeshRenderer>();
         float fadeAlpha = 1f;
         Color fadeColor = meshRenderer.material.color;
 
-        float pickUpTimer = 1f;
+        float pickUpTimer = 10f;
 
+        rb.velocity = Vector3.zero;
         while (pickUpTimer > 0 && isReachedToPlayer == false)
         {
             // 아이템과 플레이어 사이 거리
-            float distance = (Vector3.Distance(player.transform.position, rb.position));
-            // 아이템이 플레이어에게 도달함
+            float distance = (Vector3.Distance(player.transform.position + playerOffset, rb.position));
+
+            // 아이템이 플레이어게 도달함
             if (distance < 0.1f)
             {
                 isReachedToPlayer = true;
+                rb.velocity = Vector3.zero;
+                break;
             }
 
-            // 아이템 -> 플레이어로 날아갈 속도 벡터
-            Vector3 moveVec = player.transform.position - rb.position;
+            // 아이템 -> 플레이어 로 날아갈 속도 벡터
+            Vector3 moveVec = player.transform.position + playerOffset - rb.position;
 
-            // 플레이어에게 날아갈 속도
-            rb.position += moveVec * Time.deltaTime;
+            Debug.Log(moveVec);
+            // 날아가자
+            //transform.position += moveVec * Time.deltaTime; // 단순 transform 포지션 변경, rb 포지션과 속도 연산 추가 필요
+            //rb.position += moveVec * Time.deltaTime; // 단순 rb 포지션 변경
+            rb.MovePosition(rb.position + moveVec * Time.deltaTime); // 선형보간하면서 rb 포지션 변경
 
             // 색변경
             fadeAlpha -= Time.deltaTime;
             fadeColor = new Color(fadeColor.r, fadeColor.g, fadeColor.b, fadeAlpha);
             meshRenderer.material.color = fadeColor;
-            
+
             pickUpTimer -= Time.deltaTime;
             yield return null;
         }
+
         Destroy(gameObject);
-        
     }
+
 }
